14d74fe0456973692eb63ede73cc7dc89ed3f365
ljmin <ljmin@DESKTOP-AAOGLAC>
2019-06-23 13:54:38 +0900

    최초 커밋

---
+++ src/com/test/TestOneClass.java
@@ -0,0 +1,8 @@
+package com.test;
+
+public class TestOneClass {
+
+	public static void main(String[] args) {
+		System.out.println("Test One !!");
+	}
+}
---
+++ src/com/test/TestTwoClass.java
@@ -0,0 +1,9 @@
+package com.test;
+
+public class TestTwoClass {
+
+	public static void main(String[] args) {
+		System.out.println("Test Two !!");
+	}
+
+}
